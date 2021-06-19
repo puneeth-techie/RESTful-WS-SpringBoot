5ce1b015c4d197cf4f453c9169f9c69893e4df14
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-06-19 16:58:19 +0530

    Implmented JPA CrudRepository.

---
+++ src/main/java/com/rest/ws/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.rest.ws.repository;
+
+import com.rest.ws.io.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepo extends CrudRepository<UserEntity, Long> {
+}
