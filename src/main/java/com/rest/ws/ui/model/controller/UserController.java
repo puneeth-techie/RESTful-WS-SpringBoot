4048f7eb068b58046a1ce75344ded3761ac7e216
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-06-20 20:25:32 +0530

    Added Support for both XMLand JSON response and request format.

--- src/main/java/com/rest/ws/ui/model/controller/UserController.java
+++ src/main/java/com/rest/ws/ui/model/controller/UserController.java
@@ -6,6 +6,7 @@ import com.rest.ws.ui.model.request.UserRequestModel;
 import com.rest.ws.ui.model.response.UserResponseModel;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -20,14 +21,14 @@ public class UserController {
     @Autowired
     UserResponseModel userResponseModel;
 
-    @GetMapping("/{userId}")
+    @GetMapping(value = "/{userId}", produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserResponseModel getUser(@PathVariable String userId){
         UserDto user = userService.getUserById(userId);
         BeanUtils.copyProperties(user, userResponseModel);
         return userResponseModel;
     }
 
-    @PostMapping
+    @PostMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserResponseModel createUser(@RequestBody UserRequestModel userDetails) {
         BeanUtils.copyProperties(userDetails, userDto);
         UserDto createdUser = userService.createUser(userDto);
