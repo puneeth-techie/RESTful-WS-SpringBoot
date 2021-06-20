9984b1b12d0102af8ab3597c4f2c19df6e750480
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-06-20 18:26:43 +0530

    Added Authorization filter to WebSecurity.

--- src/main/java/com/rest/ws/security/WebSecurity.java
+++ src/main/java/com/rest/ws/security/WebSecurity.java
@@ -32,7 +32,8 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .anyRequest()
                 .authenticated()
                 .and()
-                .addFilter(new AuthenticationFilter(authenticationManager()));
+                .addFilter(new AuthenticationFilter(authenticationManager()))
+                .addFilter(new AuthorizationFilter(authenticationManager()));
     }
 
     @Override
