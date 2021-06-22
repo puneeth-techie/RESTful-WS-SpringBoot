1d17495f7612444c6a6231e03799be6228c1f75f
puneeth-techie <puneethpgowda.techie@gmail.com>
2021-06-22 13:35:36 +0530

    Added Model Mapper for deep mapping of objects.

--- src/main/java/com/rest/ws/ui/model/controller/UserController.java
+++ src/main/java/com/rest/ws/ui/model/controller/UserController.java
@@ -6,6 +6,7 @@ import com.rest.ws.ui.model.request.UserRequestModel;
 import com.rest.ws.ui.model.response.OperationNames;
 import com.rest.ws.ui.model.response.OperationResult;
 import com.rest.ws.ui.model.response.UserResponseModel;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -62,7 +63,9 @@ public class UserController {
      */
     @PostMapping(produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE})
     public UserResponseModel createUser(@RequestBody UserRequestModel userDetails) {
-        BeanUtils.copyProperties(userDetails, userDto);
+        //BeanUtils.copyProperties(userDetails, userDto);
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.map(userDetails, userDto);
         UserDto createdUser = userService.createUser(userDto);
         BeanUtils.copyProperties(createdUser, userResponseModel);
         return userResponseModel;
